9105039af40e41ea54590474a3d12137566fc32d
Clovin <fudeoagr12@gmail.com>
2025-11-21 09:42:12 -0300

    Remove commented seed section headers in Main.java
    Cleaned up Main.java by removing commented section headers for seed data and menu initialization to improve code readability.

--- GameStore/src/br/com/gamestore/App/Main.java
+++ GameStore/src/br/com/gamestore/App/Main.java
@@ -16,17 +16,17 @@ public class Main {
         ClienteService clienteService = new ClienteService();
         VendaService vendaService = new VendaService(produtoService);
 
-        // seed - alguns produtos de exemplo
+
         produtoService.adicionarProduto(new Console("C-PS5", "PlayStation 5", "Console PS5 - 825GB", 3999.00, 5, "Sony"));
         produtoService.adicionarProduto(new Jogo("J-GOW", "God of War Ragnarok", "Ação/Aventura", 249.00, 10, "PS5"));
         produtoService.adicionarProduto(new Acessorio("A-HX", "Headset HyperX", "Headset Gamer", 349.90, 8, "Headset"));
         produtoService.adicionarProduto(new Jogo("J-FH5", "Forza Horizon 5", "Corrida", 179.90, 6, "Xbox"));
         produtoService.adicionarProduto(new Console("C-SW", "Nintendo Switch", "Switch OLED", 2999.00, 3, "Nintendo"));
 
-        // seed - cliente exemplo
+
         clienteService.adicionarCliente(new Cliente("cli01", "David Silva", "david@email.com", "000.000.000-00"));
 
-        // inicia menu
+
         Menu menu = new Menu(produtoService, clienteService, vendaService);
         menu.iniciar();
     }
