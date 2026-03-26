b1f0fe6c76e84df427cf095a4a32fd978b8727ba
Clovin <fudeoagr12@gmail.com>
2026-03-26 11:50:51 -0300

    feat: Initialize new Spring Boot project with Maven configuration and main application class.

---
+++ GameStore/src/br/Main/GameStoreApplication.java
@@ -0,0 +1,11 @@
+package br.Main;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GameStoreApplication.class, args);
+    }
+
+}
